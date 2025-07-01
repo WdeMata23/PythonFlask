8028caf7684bc74f6e68d610fed1cd141a047803
Wilmer de Mata <wilmerdemata149@gmail.com>
2025-07-01 10:53:44 -0600

    Primeros archivos del proyecto

---
+++ products.py
@@ -0,0 +1,5 @@
+productos = [
+    {"nombre": "laptop", "precio": 4599, "cantidad": 4},
+    {"nombre": "Monitor", "precio": 599, "cantidad": 7},
+    {"nombre": "Teclado", "precio": 199, "cantidad": 3},
+]
