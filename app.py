2f8139772b8b6bb1bddf47f0d6dde4813ad7aa44
Wilmer de Mata <wilmerdemata149@gmail.com>
2025-07-01 11:14:16 -0600

    Creacion de metodos GET

--- app.py
+++ app.py
@@ -10,5 +10,10 @@ def ping():
     return jsonify({"message": "prueba"})
 
 
+@app.route("/productos")
+def getProductos():
+    return jsonify({"productos": productos, "message": "lista de productos"})
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
