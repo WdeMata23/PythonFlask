8a3060640cd1108e626ee899991795660b2d8105
Wilmer de Mata <wilmerdemata149@gmail.com>
2025-07-01 11:45:13 -0600

    Ingreso de metodos post y put

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -13,6 +13,37 @@ def ping():
 @app.route("/productos")
 def getProductos():
     return jsonify({"productos": productos, "message": "lista de productos"})
+
+
+@app.route("/productos/<string:product_nombre>")
+def getProducto(product_nombre):
+    productosFound = [
+        producto for producto in productos if producto["nombre"] == product_nombre
+    ]
+    return jsonify({"producto": productosFound[0]})
+
+
+@app.route("/productos", methods=["POST"])
+def agregarproducto():
+    nuevo_producto = {
+        "nombre": request.json["nombre"],
+        "precio": request.json["precio"],
+        "cantidad": request.json["cantidad"],
+    }
+    productos.append(nuevo_producto)
+    return "recibido"
+
+
+@app.route("/productos/<string:product_nombre>", methods=["PUT"])
+def actualizarproducto(product_nombre):
+    productosFound = [
+        producto for producto in productos if producto["nombre"] == product_nombre
+    ]
+    if len(productosFound) > 0:
+        productosFound[0]["nombre"] == request.json["nombre"],
+        productosFound[0]["precio"] == request.json["precio"],
+        productosFound[0]["cantidad"] == request.json["cantidad"]
+        return jsonify({"message": "producto actualizado"})
 
 
 if __name__ == "__main__":
