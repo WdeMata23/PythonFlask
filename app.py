ea9e3b01775aed45bac28f95d6c91b3be9573c6f
Wilmer de Mata <wilmerdemata149@gmail.com>
2025-07-01 12:12:16 -0600

    App terminada

--- app.py
+++ app.py
@@ -43,7 +43,21 @@ def actualizarproducto(product_nombre):
         productosFound[0]["nombre"] == request.json["nombre"],
         productosFound[0]["precio"] == request.json["precio"],
         productosFound[0]["cantidad"] == request.json["cantidad"]
-        return jsonify({"message": "producto actualizado"})
+        return jsonify(
+            {"message": "producto actualizado", "producto": productosFound[0]}
+        )
+    return jsonify({"message": "producto no encontrado"})
+
+
+@app.route("/productos/<string:product_nombre>", methods=["DELETE"])
+def Eliminarprodu(product_nombre):
+    productosFound = [
+        producto for producto in productos if producto["nombre"] == product_nombre
+    ]
+    if len(productosFound) > 0:
+        productos.remove(productosFound)
+        return jsonify({"message": "producto Eliminado"})
+    return jsonify({"message": "producto no encontrado"})
 
 
 if __name__ == "__main__":
